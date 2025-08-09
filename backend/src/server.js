70c4665736a17992d11a392b0821ea44129e1907
刘鹏 <2254842466@qq.com>
2025-08-10 00:13:56 +0800

    user service

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,35 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
+
+
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is up and running on PORT:", 5001));
+app.use(cors())
+app.use(express.json())
+
+app.use(clerkMiddleware())
+
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.use("/api/users", userRoutes);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    // listen for local development
+    if (ENV.NODE_ENV !== 'production') {
+      app.listen(ENV.PORT, () => console.log('Server is up and running on PORT:', ENV.PORT));
+    }
+  } catch (error) {
+    console.error('Failed to start server:', error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
